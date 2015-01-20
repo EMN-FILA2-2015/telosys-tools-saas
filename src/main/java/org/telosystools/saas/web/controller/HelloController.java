853e8b0e686cbfcc03ca119cd6ab0da9904bc288
Ludovic Chaboud <l.chaboud@gmail.com>
2015-01-20 17:50:42 +0100

    hello

--- src/main/java/org/telosystools/saas/web/controller/HelloController.java
+++ src/main/java/org/telosystools/saas/web/controller/HelloController.java
@@ -1,7 +1,7 @@
 package org.telosystools.saas.web.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.telosystools.saas.domain.Hello;
 
@@ -10,9 +10,10 @@ public class HelloController {
 
     private static final String template = "Hello, %s!";
 
-    @RequestMapping("/hello")
-    public Hello hello(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping("/hello/{name}")
+    public Hello hello(@PathVariable("name") String name) {
         return new Hello(1,
                 String.format(template, name));
     }
+
 }
