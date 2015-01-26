190fa6a785381243b8cd47d445fe9bdfeca568c4
bwilcox <wilcox.benoit@gmail.com>
2015-01-26 22:18:03 +0100

    **tech** Init stack applicative
    - Mise en place du logger Logback
    - Mise en oeuvre des fichiers de properties (par profil)
    - Gestion des profils maven et spring
    - Mise en oeuvre d'un TU pour le service REST example

---
+++ src/main/java/org/telosystools/saas/config/Constants.java
@@ -0,0 +1,11 @@
+package org.telosystools.saas.config;
+
+/**
+ * Created by BWI on 26/01/15.
+ */
+public final class Constants {
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+
+}
